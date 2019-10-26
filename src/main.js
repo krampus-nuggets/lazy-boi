cb656dde8a394ccffdad8b02b5f4efd4bce5db66
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-27 01:40:24 +0200

    MAIN -> func conv async func

--- src/main.js
+++ src/main.js
@@ -7,3 +7,6 @@ import execa from 'execa';
 import Listr from 'listr';
 import { projectInstall } from 'pkg-install';
 
+// Convert to fs.access to Async Function | Assign to access variable
+const access = promisify(fs.access);
+
